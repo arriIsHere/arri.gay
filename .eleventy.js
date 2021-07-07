743987bcb236bdb4c7d13a99d2b46352c7097d2d
Arriana Blais <arriblais@gmail.com>
2021-07-06 20:29:37 -0400

    copy repo from old one

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+const Moment = require('moment');
+const SyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
+module.exports = function(eleventyConfig) {
+
+	// Install plugin for syntax highlighting
+	eleventyConfig.addPlugin(SyntaxHighlight);
+
+	eleventyConfig.addFilter('date', function(value, format) {
+		return Moment(value).format(format);
+	});
+
+	return {
+		dir: {
+			input: 'src',
+			output: 'dist'
+		},
+		templateFormats: [
+			"md",
+			"html",
+			"css",
+			"jpg",
+			"png",
+			"gif",
+			"njk"
+		],
+		passthroughFileCopy: true
+	}
+};
---
+++ src/articles/timetravel-and-cocoons/index.md
@@ -0,0 +1,7 @@
+---
+title: Timetravel and Cocoons
+date: 2021-07-06
+abstract: Time is a fricle thing, it moves forward never back, but what if it did?
+abstractAuthor: name of author
+image: header.jpg
+---
